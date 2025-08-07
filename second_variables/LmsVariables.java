6a561223b8674f0aacdf84e044aacfcb15f9664a
althaf964 <tappaalthaf6469@gmail.com>
2025-08-07 22:29:15 +0530

    Variables&datatype program

---
+++ second_variables/LmsVariables.java
@@ -0,0 +1,16 @@
+package second_variables;
+
+public class LmsVariables {
+    public static void main(String[] args) {
+        Short StudentId = 100;
+        String StudentName = "Althaf";
+        String StudentEmail = "althaf@gmail.com";
+        String StudentPassword = "alth123";
+        long studentmobilenumber = 900090;
+        boolean isStudentMale = true;
+        System.out.println("Student Id"+StudentId);
+        System.out.println("Student Name" +StudentName +StudentEmail);
+        System.out.println("Student mobile number" +studentmobilenumber );
+        System.out.println("student gender Male" +isStudentMale);
+    }
+}
---
+++ second_variables/VariablesDemo.java
@@ -0,0 +1,15 @@
+package second_variables;
+
+public class VariablesDemo {
+    static int someData; // instance variables
+    String brand = "Highlander";
+    String description ="nice fit";
+    double ratings = 4.2;
+    short price = 311;
+ public static void main(String[] args) {
+      int someDataNew1 = 10; //initialization
+      int someDataNew2; 
+    System.out.println(someDataNew1); //local variables
+    System.out.println(someData);
+ }
+}
