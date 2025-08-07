e770c21f8ab4caf52f0609aedd2754103840dc00
althaf964 <tappaalthaf6469@gmail.com>
2025-08-07 23:13:15 +0530

    application.program

---
+++ second_variables/Appliction.java
@@ -0,0 +1,26 @@
+package second_variables;
+
+public class Appliction {
+    public static void main(String[] args) {
+        String FirstName = "Althaf";
+        String MiddleName = "Althu";
+        String Lastname = "Tappa";
+        String DateOfBirth = "22/Dec/2003";
+        boolean ismale = true;
+        long ContactNumber = 90078;
+        String FatherName = "Dadapeer";
+        String MotherName = "Rahamatha";
+        short yearGraduated = 2021;
+        double average = 9.7;
+        System.out.println("First Name" +FirstName);
+        System.out.println("Middle Name" +MiddleName);
+        System.out.println("Last Name" +Lastname);
+        System.out.println("Date Of Birth" +DateOfBirth);
+        System.out.println("isMale" +ismale);
+        System.out.println("ContactNumber" +ContactNumber);
+        System.out.println("Father Name" +FatherName); 
+        System.out.println("Mother Name" +MotherName);
+        System.out.println("yearGraduated" +yearGraduated);
+        System.out.println("average" +average);                                                                                                                                                                                                                
+    }
+}
