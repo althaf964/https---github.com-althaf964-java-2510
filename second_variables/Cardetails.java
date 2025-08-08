d319f2ee2a5a4876f4a6823016b74fc2cac3b79a
althaf964 <tappaalthaf6469@gmail.com>
2025-08-08 21:52:09 +0530

    Car.program

---
+++ second_variables/Cardetails.java
@@ -0,0 +1,29 @@
+package second_variables;
+
+public class Cardetails {
+    public static void main(String[] args) {
+        String EngineType = "mHAWK 4 Cylinders";
+        String Displacement = "2184 cc";
+        String MaxPower = "130bhp@3750rpm";
+        String MaxTorque = "300Nm@1600-2800";
+        byte NoOfCylinders = 4;
+        byte ValvesPerCylinder = 4;
+        String FuelSupplySystem = "CRDi";
+        boolean isturbocharger = true;
+        String TransmissionType = "Manual";
+        String Gearbox = "6-speed";
+        String DriverType = "RWD";
+        System.out.println("EngineType" +EngineType);
+        System.out.println("Displacement" +Displacement);
+        System.out.println("MaxPower" +MaxPower);
+        System.out.println("MaxTorque" +MaxTorque);
+        System.out.println("NoOfCylinders" +NoOfCylinders);
+        System.out.println("ValvesPerCylinder" +ValvesPerCylinder);
+        System.out.println("FuelSupplySystem" +FuelSupplySystem);
+        System.out.println("isturbocharger" +isturbocharger);
+        System.out.println("TransmissionType" +TransmissionType);
+        System.out.println("Gearbox" +Gearbox);
+        System.out.println("DriverType" +DriverType);   
+    }
+
+}
